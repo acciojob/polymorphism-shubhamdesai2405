9dd8dfebb071a80f65d75f8f781a637a2a0da104
Shubham <shubhamdesai2405@gmail.com>
2022-10-09 21:13:26 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,16 @@
 package com.driver;
 
 public class Main {
-static class Product{
+public static class Product{
     public int product(int x, int y) {
-      return x+y;
+      return x*y;
     }
     public int product(int x, int y, int z) {
-         return x+y+z;
+         return x*y*z;
     }
     public double product(double x, double y) {
 
-     return x+y;
+     return x*y;
     }
 }
 
@@ -19,11 +19,13 @@ static class Product{
         int x=0,y=0,z=0;
         double a=0,b=0;
 
-        p.product(x,y);
+        System.out.println(p.product(x,y));
 
-        p.product(x,y,z);
+        System.out.println(p.product(x,y,z));
 
-        p.product(a,b);
+        System.out.println(p.product(a,b));
+
+
 
     }
 
